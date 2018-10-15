2ccc8850794d087f9740385e4dab2f4f53032880
yomo-kun <37106683+yomo-kun@users.noreply.github.com>
2018-10-15 12:34:18 +0200

    Basis

---
+++ TowerDefense/TowerDefense/Assets/Scripts/Bullets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour {
+
+    int Attackdamage = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  
+}
---
+++ TowerDefense/TowerDefense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    int Damage = 5;
+    int health = 50;
+    int coin = 1;
+
+	void Start () {
+		
+	}
+	
+
+
+    public void dropCoin()
+    {
+
+    }
+	// Update is called once per frame
+	void Update () {
+
+        
+		
+	}
+}
